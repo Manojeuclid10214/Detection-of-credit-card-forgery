17f684e0d154b9c87f86db5e72d413ce0c80c20b
Manojeuclid10214 <42431832+Manojeuclid10214@users.noreply.github.com>
2018-08-16 15:24:23 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import Linear Regression
 from sklearn.cross_validation import KFold, cross_val_score
 from sklearn.metrics import confusion_matrix, precision_recall_curve, auc, roc_auc_score, roc_curve, recall_score, classification_report
 import itertools
@@ -14,19 +16,17 @@ from sklearn.neighbors import KNeighborsClassifier
 
 show_bdry = False
 show_best_c = False
-# Either all features in the feature sets are normalized or only the amount value is normalized
+
 def normalize_feature(data, amount_only = False):
     if amount_only:
         data['Amount'] = StandardScaler().fit_transform(data['Amount'].values.reshape(-1,1))
+        data.head()
     else:
         for feature in data.columns[:-1]:
             data[feature] = StandardScaler().fit_transform(data[feature].values.reshape(-1,1))
-    return data
+   return data
 
-#Data are splitted to train and test sets: in each set
-#Let total_normal and total_fraud be the total numbers of normal and fraud activities in the data sets.
-#The train set contains (1-test_size)*total_normal normal activities and  (1-test_size)*total_fraud fraud activities.
-#The test set contains test_size*total_normal normal activities and  test_size*total_fraud fraud activities.
+
 def split_train_test(fraud_indices, normal_indices, test_size = 0.3):
     number_records_fraud = len(fraud_indices)
     number_records_normal = len(normal_indices)
@@ -41,18 +41,18 @@ def split_train_test(fraud_indices, normal_indices, test_size = 0.3):
 
     return train_normal_indices, train_fraud_indices, test_normal_indices, test_fraud_indices
 
-#Train set is downsampled so that in the train set the number of normal activities = ratio*the number of normal activities
 def getTrainingSample(train_fraud_indices, train_normal_indices, data, train_normal_pos, ratio):
     train_number_records_fraud = int(ratio*len(train_fraud_indices))
     train_number_records_normal = len(train_normal_indices)
     if train_normal_pos + train_number_records_fraud <= train_number_records_normal:
         small_train_normal_indices = train_normal_indices[train_normal_pos: train_normal_pos + train_number_records_fraud]
         train_normal_pos = train_normal_pos + train_number_records_fraud
+        
     else:
         small_train_normal_indices = np.concatenate([train_normal_indices[train_normal_pos: train_number_records_normal],train_normal_indices[0: train_normal_pos + train_number_records_fraud- train_number_records_normal]])
         train_normal_pos = train_normal_pos + train_number_records_fraud - train_number_records_normal
 
-    # Appending the 2 fraud_indices
+   
     under_train_sample_indices = np.concatenate([train_fraud_indices, small_train_normal_indices])
     np.random.shuffle(under_train_sample_indices)
     # Under sample dataset
@@ -66,33 +66,33 @@ def getTrainingSample(train_fraud_indices, train_normal_indices, data, train_nor
 # It is a module of k-nearest nbd. which is called in cross-validation.
 def knn_module(X, y, indices, c_param, bdry = None):
     knn = KNeighborsClassifier(n_neighbors = c_param)
-    knn.fit(X.iloc[indices[0],:], y.iloc[indices[0],:].values.ravel())
-    y_pred_undersample = knn.predict(X.iloc[indices[1],:].values)
+    knn.fit(X.iloc[indices[0],:], y.iloc[indices[1],:].values.ravel())
+    y_pred_undersample = knn.predict(X.iloc[indices[0],:].values)
     return y_pred_undersample
 
 # It is a module of support vector machine using Gaussian radix basis function kernel which is called in cross-validation.
-def svm_rbf_module(X, y, indices, c_param, bdry = 0.5):
+def svm_rbf_module(X, y, indices, c_param, bdry = 0.7):
     svm_rbf = SVC(C=c_param, probability = True)
     svm_rbf.fit(X.iloc[indices[0],:], y.iloc[indices[0],:].values.ravel())
     y_pred_undersample = svm_rbf.predict_proba(X.iloc[indices[1],:].values)[:,1]>=bdry
     return y_pred_undersample
 
 # It is a module of support vector machine using polynomial kernel which is called in cross-validation.
-def svm_poly_module(X, y, indices, c_param, bdry = 0.5):
+def svm_poly_module(X, y, indices, c_param, bdry = 0.7):
     svm_poly = SVC(C= c_param[0], kernel = 'poly', degree = c_param[1], probability = True)
     svm_poly.fit(X.iloc[indices[0],:], y.iloc[indices[0],:].values.ravel())
     y_pred_undersample = svm_poly.predict_proba(X.iloc[indices[1],:].values)[:,1]>=bdry
     return y_pred_undersample
 
 # It is a module of logistic regression which is called in cross-validation.
-def lr_module(X, y, indices, c_param, bdry = 0.5):
-    lr = LogisticRegression(C = c_param, penalty = 'l1')
+def lr_module(X, y, indices, c_param, bdry = 0.7):
+    lr = LogisticRegression(C = c_param, penalty = '12')
     lr.fit(X.iloc[indices[0],:], y.iloc[indices[0],:].values.ravel())
     y_pred_undersample= lr.predict_proba(X.iloc[indices[1],:].values)[:,1]>=bdry
     return y_pred_undersample
 
 # It is a module of random forest which is called in cross-validation
-def rf_module(X, y, indices, c_param, bdry = 0.5):
+def rf_module(X, y, indices, c_param, bdry = 0.7):
     rf = RandomForestClassifier(n_jobs=-1, n_estimators = 100, criterion = 'entropy', max_features = 'auto', max_depth = None, min_samples_split  = c_param, random_state=0)
     rf.fit(X.iloc[indices[0],:], y.iloc[indices[0],:].values.ravel())
     y_pred_undersample = rf.predict_proba(X.iloc[indices[1],:].values)[:,1]>=bdry
@@ -119,6 +119,7 @@ def cross_validation_recall(x_train_data,y_train_data, c_param_range, models_dic
 
     results_table = pd.DataFrame(index = range(len(c_param_range),2), columns = ['C_parameter','Mean recall score'])
     results_table['C_parameter'] = c_param_range
+    results_table.head()
 
     recall_mean = []
     for c_param in c_param_range:
@@ -144,7 +145,7 @@ def decision_boundary(x_train_data,y_train_data, fold,  best_c, bdry_dict, model
     bdry_ranges = [0.3, 0.35, 0.4, 0.45, 0.5]
     results_table = pd.DataFrame(index = range(len(bdry_ranges),2), columns = ['C_parameter','Mean recall score * auc'])
     results_table['Bdry_params'] = bdry_ranges
-
+     results.options.mode.chained_assignment = None
     recall_mean = []
     for bdry in bdry_ranges:
         recall_accs_aucs = []
@@ -156,7 +157,7 @@ def decision_boundary(x_train_data,y_train_data, fold,  best_c, bdry_dict, model
 
     results_table['Mean recall score * auc'] = recall_mean
     best_bdry = results_table.loc[results_table['Mean recall score * auc'].idxmax()]['Bdry_params']
-
+    best_bdry.options.mode.chained_assignment = None
     return best_bdry
 
 # Model method is our model to classify the fraud and normal activities.
@@ -173,7 +174,7 @@ def model(X, y, train, bdry_dict = None, best_c = None, best_bdry = None, models
         best_c_knn = cross_validation_recall(X, y, c_param_range_knn, models_dict, 'knn')
 
         # The penalty parameter in the support vector machine using Gaussian radix basis function kernel is determined.
-        c_param_range_svm_rbf = [0.01, 0.1, 1, 10, 100]
+        c_param_range_svm_rbf = [0.01, 0.1, 1, 12, 100]
         best_c_svm_rbf = cross_validation_recall(X, y, c_param_range_svm_rbf, models_dict, 'svm_rbf')
         c_param_range_svm_poly = [[0.01, 2], [0.01, 3], [0.01, 4], [0.01, 5], [0.01, 6], [0.01, 7], [0.01, 8], [0.01, 9],
                                   [0.1, 2], [0.1, 3], [0.1, 4], [0.1, 5], [0.1, 6], [0.1, 7], [0.1, 8], [0.1, 9],
@@ -189,9 +190,10 @@ def model(X, y, train, bdry_dict = None, best_c = None, best_bdry = None, models
         c_param_range_lr = [0.01,0.1,1,10,100]
         best_c_lr = cross_validation_recall(X, y, c_param_range_lr, models_dict, 'lr')
         # The min_samples_split in the random forest model is determined
-        c_param_range_rf = [2, 5, 10, 15, 20]
+        c_param_range_rf = [2, 5, 12, 15, 20]
         best_c_rf = cross_validation_recall(X, y, c_param_range_rf, models_dict, 'rf')
         best_c = [best_c_knn, best_c_svm_rbf, best_c_svm_poly, best_c_lr, best_c_rf, best_c]
+        
 
         # Using cross-validaton, decision boundary thresholds in the following learning models are determined
         # by optimizing some function of auc and recall score in the model.
@@ -209,7 +211,7 @@ def model(X, y, train, bdry_dict = None, best_c = None, best_bdry = None, models
 
         # The decision boundary threshold of the logistic regression model is determined.
         best_bdry_lr = decision_boundary(X, y, fold, best_c_lr, bdry_dict, models_dict, 'lr')
-
+        best_bdry_lr.options.mode.chained_assignment = None
         # The decision boundary threshold of the random forest model is determined.
         best_bdry_rf = decision_boundary(X, y, fold, best_c_lr, bdry_dict, models_dict, 'rf')
         best_bdry = [0.5, best_bdry_svm_rbf, best_bdry_svm_poly, best_bdry_lr, best_bdry_rf]
@@ -231,7 +233,7 @@ def model(X, y, train, bdry_dict = None, best_c = None, best_bdry = None, models
         rf.fit(X.values, y.values.ravel())
 
         models = [knn, svm_rbf, svm_poly, lr, rf]
-
+        models.head()
         return best_c, best_bdry, models
     else:
         # This part is to classify fraud and normal activities in a test set by our model.
@@ -253,7 +255,7 @@ def model(X, y, train, bdry_dict = None, best_c = None, best_bdry = None, models
 
         x_of_three_models = {'knn' : y_pred_knn, 'svm_rbf' : y_pred_svm_rbf, 'svm_poly' : y_pred_svm_poly, 'lr' : y_pred_lr, 'rf': y_pred_rf}
         X_5_data = pd.DataFrame(data = x_of_three_models)
-
+        print( X_5_data)
         y_pred= np.sum(X_5_data, axis = 1)>=mode
 
         y_pred_lr_controls = []
@@ -274,7 +276,7 @@ def run(data, mode, ratio, iteration1, bdry_dict):
     best_c = None
     best_bdry = None
     for itr1 in range(iteration1):
-        print("percentage: %.2f" %(itr1/iteration1*100))
+        print("percentage: %.4f" %(itr1/iteration1*100))
 
         # Number of data points in the minority class
         fraud_indices = np.array(data[data.Class == 1].index)
@@ -344,6 +346,7 @@ def run(data, mode, ratio, iteration1, bdry_dict):
 
     result = [mean_recall_score, std_recall_score, mean_auc, std_auc]
     control = [mean_recall_score_lr, std_recall_score_lr, mean_auc_lr, std_auc_lr]
+    control.head(()
     return result, control, params
 
 
@@ -366,7 +369,7 @@ def rf_bdry_module(recall_acc, roc_auc):
 
 bdry_dict = {'lr': lr_bdry_module,'svm_rbf': svm_rbf_bdry_module,
              'svm_poly': svm_poly_bdry_module, 'rf': rf_bdry_module}
-################################################################################
+bdry_dict.head()
 
 # Implementation of our model according to these parameters setting.:
 data = pd.read_csv("creditcard.csv")
@@ -386,4 +389,5 @@ for i, param in enumerate(params):
     print("Threshold", param)
     print("mean_recall is ", control[0][i], " and std is ", control[1][i])
     print("mean_auc is ", control[2][i], " and std is ", control[3][i])
+                 
 print()
